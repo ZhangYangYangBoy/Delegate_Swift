5cd704409e8967b8a2d85caf0eb3cb0fa3e8f264
yangyangzhang <zyydeveloper@163.com>
2017-05-08 16:05:02 +0800

    finished develop delegate swift version

--- Delegate_Swift/ViewController.swift
+++ Delegate_Swift/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+@objc protocol HeaderDelegate {
+    func buttonClick(str: String)
+}
 
+class CustomView: UIView {
+   weak var delegate:HeaderDelegate?
+   private var submitBtn: UIButton?
+    
+   override init(frame: CGRect) {
+     super.init(frame: frame)
+     submitBtn = UIButton(type: .custom)
+     submitBtn?.frame = CGRect.init(x: 10, y: 30, width: 100, height: 50)
+     submitBtn?.backgroundColor = UIColor.red
+     submitBtn?.setTitle("代理传值", for: .normal)
+     submitBtn?.addTarget(self, action: #selector(clickAction(_ :)), for: .touchUpInside)
+     self.addSubview(submitBtn!)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func clickAction(_ : UIButton)  {
+       self.delegate?.buttonClick(str:"ffff")
+    }
+}
+
+class ViewController: UIViewController,HeaderDelegate {
+    
+    var customView: CustomView?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.customView = CustomView.init(frame: CGRect.init(x: 0, y: 0, width:self.view.frame.size.width , height: 200))
+        self.customView?.backgroundColor = UIColor.green
+        self.customView?.delegate = self
+        self.view.addSubview(self.customView!)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    internal func buttonClick(str: String) {
+        print(str)
     }
-
-
 }
 
